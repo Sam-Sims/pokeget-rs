16a22c77db31e0e0455bceee51097e312f98cbb8
sam <samsimss98@gmail.com>
2025-04-30 18:42:34 +0100

    feat: add stdin support for pokemon input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! Display pokemon sprites in your terminal.
 
+use std::io::{BufReader, Read};
 use clap::Parser;
 use pokeget::cli::Args;
 use pokeget::list::List;
@@ -11,14 +12,28 @@ fn main() {
     let list = List::read();
     let args = Args::parse();
 
-    if args.pokemon.is_empty() {
+    let attributes = Attributes::new(&args);
+
+    let input_pokemon = if args.pokemon[0] == "-" {
+        let stdin = std::io::stdin();
+        let mut reader = BufReader::new(stdin.lock());
+        let mut buf = String::new();
+
+        if let Err(e) = reader.read_to_string(&mut buf) {
+            eprintln!("Error reading from stdin: {}", e);
+            exit(1);
+        }
+        buf.split_whitespace().map(|x| x.to_string()).collect()
+    } else {
+        args.pokemon
+    };
+
+    if input_pokemon.is_empty() {
         eprintln!("you must specify the pokemon you want to display");
         exit(1);
     }
 
-    let attributes = Attributes::new(&args);
-    let pokemons: Vec<Pokemon> = args
-        .pokemon
+    let pokemons: Vec<Pokemon> = input_pokemon
         .into_iter()
         .map(|x| Pokemon::new(x, &list, &attributes))
         .collect();
